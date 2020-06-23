da2f1a6a2c4644ff1f168d2e4e28018bfab1500a
Nerry <108566+neri@users.noreply.github.com>
2020-06-24 03:49:23 +0900

    Fix build script

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
 
     Command::new("nasm")
-        .args(&["-f", "win64", "src/myos/arch/asm.asm", "-o"])
+        .args(&["-f", "win64", "src/kernel/arch/asm.asm", "-o"])
         .arg(&format!("{}/asm.o", out_dir))
         .status()
         .unwrap();
@@ -19,5 +19,5 @@ fn main() {
 
     println!("cargo:rustc-link-search=native={}", out_dir);
     println!("cargo:rustc-link-lib=static=myos");
-    println!("cargo:rerun-if-changed=src/myos/arch/*.asm");
+    println!("cargo:rerun-if-changed=src/kernel/arch/*.asm");
 }
